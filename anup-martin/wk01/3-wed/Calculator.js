611684e16fcbd43cd613f7db123630e57257452e
anuprodney.martin <anuprodney.martin@gmail.com>
2022-06-22 17:38:07 +1000

    Homework done for Wednesday

---
+++ anup-martin/wk01/3-wed/Calculator.js
@@ -0,0 +1,18 @@
+
+
+
+const squareNumber = function(num){
+    square = (num * num);
+    console.log(`when we want a square of a num ${ square }`)
+};
+    squareNumber (13);
+
+
+
+
+    
+//     Write a function called squareNumber that will take one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+// Write a function called halfNumber that will take one argument (a number), divide it by 2, and return the result. It should also log a string like "Half of 5 is 2.5.".
+// Write a function called percentOf that will take two numbers, figure out what percent the first number represents of the second number, and return the result. It should also log a string like "2 is 50% of 4."
+// Write a function called areaOfCircle that will take one argument (the radius), calculate the area based on that, and return the result. It should also log a string like "The area for a circle with radius 2 is 12.566370614359172."
+// Bonus: Round the result so there are only two digits after the decimal.
