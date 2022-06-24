dd7b018b51db50002aeaf241e5b4c2043832e6c0
rohan -fa <rohanfarzna@gmail.com>
2022-06-24 15:15:52 +1000

    YOUR_COMMIT_MESSAGE_GOES_HERE

--- rohan-farzana/wk01/3-wed/js/homework2.js
+++ rohan-farzana/wk01/3-wed/js/homework2.js
@@ -3,15 +3,26 @@
 // Use the if-then-else construct available in Javascript. 
 // You'll have to remember your pre-work, or do some googling to figure this out.
 
-const maxOfTwoNumbers= function(numOne,numTwo){
-    if( numOne > numTwo ){
+// const maxOfTwoNumbers= function(numOne,numTwo){
+//     if( numOne > numTwo ){
         
-        console.log(`the largest one is ${numOne} and the smallest one is ${numTwo} `); 
-    } else{
-        console.log(`the largest one is ${numTwo} and the smallest one is ${numOne} `)
+//         console.log(`the largest one is ${numOne} and the smallest one is ${numTwo} `); 
+//     } else{
+//         console.log(`the largest one is ${numTwo} and the smallest one is ${numOne} `);
+//     }
+// }
+// maxOfTwoNumbers(300,6);
+
+
+const maxOfTwoNumbers=function (num,num1){
+    if (num>num1){
+        return `${num} is the largest than ${num1}`;
+    }else if (num1>num){
+        return `${num1} is the latgest than ${num}`;
     }
 }
-maxOfTwoNumbers(300,6);
+console.log(maxOfTwoNumbers(100,5));    
+
 
 // Define a function maxOfThree that takes three numbers as arguments and 
 // returns the largest of them.
@@ -19,46 +30,24 @@ maxOfTwoNumbers(300,6);
 let theThreeNumbers=function(a,b,c){
     
     if( a<b && c<b ){
-        return b;
+        return `number ${b} is the largest one`;
     }else if (c<a){
-        return output=a;
+        return `number ${a} is the largest one`;
     }else{
-        return output=c;
+        return `number ${c} is the largest one`;
     }
 }
 
-let theNumbers= [ ];
- for( let i=0; i<3; i++){
-    theNumbers[i]=prompt(`enter number`);
- }
- console.log(`largest number is ${theNumbers}`);
-
-//  Define a function sumArray and a function multiplyArray that sums and multiplies 
-//  (respectively) all the numbers in an array of numbers. For example, 
-//  sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24
-
-let sumArray =[ 3,7,1,2];
-for (let i=0; i<sumArray.length; i++){
-    additionToBe= sumArray[i];
-    
-}
+console.log(theThreeNumbers(12,56,45));
+console.log(theThreeNumbers(20,56,45));
+console.log(theThreeNumbers(12,20,45));
 
 
-console.log(additionToBe[3,7,1,2])
 
 
-function multiNumber(numbers) {
-	var total = 1;
-	for (var i = 0; i < numbers.length; i++) {
-		total = (total * numbers[i]);
-	}
-	return total;
-}
-
-console.log(multiNumber([3,7,1,2]));
 
 
-//3. Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 
 const vowelCheck= function(chr){
     if(chr=="a" ||chr=="e" || chr=="i" || chr=="o" ||chr=="u"){
