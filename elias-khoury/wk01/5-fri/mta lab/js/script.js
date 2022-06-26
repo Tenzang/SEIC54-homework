58cc54e4ed99f935a3bfa3bbffa3b13a8593b973
Elias Khoury <eliaskhouryy@hotmail.com>
2022-06-26 14:29:58 +1000

    finished homework

--- elias-khoury/wk01/5-fri/mta lab/js/script.js
+++ elias-khoury/wk01/5-fri/mta lab/js/script.js
@@ -1,61 +1,62 @@
    
 
 const trainLines = {
-    lLine: [`1st`, `3rd`, `unionSquare`, `6th`, `8th`],
-    nLine: [`timeSquare`, `34th`, `28th`, `23rd`, `unionSquare`, `8th`],
-    sixthLine: [`grandCentral`, `33rd`, `28th`, `23rd`, `unionSquare`, `astorPlace`]
+    lLine: [`1st`, `3rd`, `Union Square`, `6th`, `8th`],
+    nLine: [`timeSquare`, `34th`, `28th`, `23rd`, `Union Square`, `8th`],
+    sixthLine: [`grandCentral`, `33rd`, `28th`, `23rd`, `Union Square`, `astorPlace`]
 };
 
 
-const lLinePlan = function (){ // produce an array for lLine
-    let x = trainLines.lLine;
-    for (let i = 0; i <= x.length; i++) {
-        return x; 
+// ---------------------------- stop array calculator and bringer ------------------------------------
+
+let stopsCount = 0;
+const stopStation = function(lines, beginStop, endStop) {
+ 
+    let linesArray = trainLines[lines];
+    let stops = ``;
+    let startStopIndex = linesArray.indexOf(beginStop);
+    let endStopIndex = linesArray.indexOf(endStop);
+
+    if (startStopIndex < endStopIndex) { //for the train stop to move forward // so if the start stop is less on the array than the end stop
+        for (let i = startStopIndex + 1; i <= endStopIndex; i++) { // +1 to the starStopIndex so it doesnt include the inital stop
+            stops = stops + linesArray[i] +`, `;
+            stopsCount++; // from 0 (the value of stopsCount) the array keeps adding
+        }
+    } else { //if the startstop is towards the end of the array and the endstop is towards the front
+        for (let i = startStopIndex - 1; i >= endStopIndex; i--) { // for the train stop to move backward
+            stops = stops + linesArray[i] + `, `;
+            stopsCount++;
+        }
     }
-    
+    return stops;
 }
-console.log(lLinePlan());
+
+// /* --------TEST 1------- */   console.log(stopStation(`lLine`, `1st`, `8th`))   /* ----------------------- */
 
 
 
-const nLinePlan = function (){ // produce an array for nLine
-    let x = trainLines.nLine;
-    for (let i = 0; i < x.length; i++) {
-        return x; 
+const planTrip = function(beginLine, beginStop, endLine, endStop) {
+    stopsCount = 0;
+    if (beginLine === endLine) {
+       let travel1 = stopStation(beginLine, beginStop, endStop);
+       console.log(`You must travel through the following stops on the ${beginLine}: ${travel1}.`);
+    } else {
+       let travel2 = stopStation(beginLine, beginStop, `Union Square`);
+       console.log(`You must travel through the following stops on the ${beginLine}: ${travel2}.`);
+       console.log(`Change at Union Square.`);
+
+       let travel3 = stopStation(endLine, `Union Square`, endStop); 
+       console.log(`Your journey continues through the following stops: ${travel3}.`) 
     }
-    
-}
-console.log(nLinePlan());
-
-
-
-const sixthLinePlan = function (){ // produce an array for nLine
-    let x = trainLines.sixthLine;
-    for (let i = 0; i < x.length; i++) {
-        return x; 
-    }
-    
-}
-console.log(sixthLinePlan());
-
-
-
-
-const planTrip = function(lineDepart, stopDepart, lineArrive, stopArrive) { // for lLine
-
-// lineDepart = lLinePlan().indexOf(`unionSquare`);
-
-if (lineDepart === `lLine` && stopDepart.indexOf() < trainLines.lLine.indexOf(`unionSquare`)) {
-    return `bitch`;
-}
-
-
-
+console.log(`${stopsCount} stops in total.`)
 };
 
-console.log(planTrip(`lLine`, `unionSquare`, ` `, `` ))
+/* -----FINAL FUNCTION--------- */       planTrip(`lLine`, `1st`, `nLine`, `23rd` )       /* ----------------------- */
+
+console.log(`-----------------------------`)
+console.log(`-----------------------------`)
+console.log(`who needs sleep anyways??`)
 
 
 
 
-// indexOf is super important to use
