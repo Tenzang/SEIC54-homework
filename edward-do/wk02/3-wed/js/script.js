089363916319979065f5dd8eeb722dc8a06b4efb
Edward Do <edwardtdo@gmail.com>
2022-06-29 21:38:40 +1000

    made the cat animation perfect

--- edward-do/wk02/3-wed/js/script.js
+++ edward-do/wk02/3-wed/js/script.js
@@ -1,30 +1,63 @@
 const cat = document.getElementsByTagName('img')[0];
 cat.style.position = 'absolute';
 cat.style.left = '0px';
-cat.style.bottom = '0px';
+cat.style.bottom = '20px';
 cat.style.width = '150px';
-cat.style.opacit = 1.0;
+cat.style.opacity = 1.0;
+
+const ufo = document.getElementById('ufo');
+ufo.style.position = 'absolute';
+ufo.style.left = '-4500px';
+ufo.style.bottom = '475px';
+ufo.style.width = '800px'
+ufo.style.zIndex = 100;
 
 let thirdTimerId = undefined;
+let fourthTimerId = undefined;
+let fifthTimerId = undefined;
+let sixthTimerId = undefined;
+let seventhTimerId = undefined;
 
-const catWalk = function (){
+const catWalk = function () {
     const oldLeft = parseInt(cat.style.left);
     const newLeft = oldLeft + 1;
     cat.style.left = newLeft + 'px';
     if (cat.style.left === '800px') {
         clearInterval(timerID);
-        thirdTimerId = setInterval(catUFOAbduct, 20);
+        thirdTimerId = setInterval(catUFOAbduct, 15);
+        fourthTimerId = setInterval(fadeAway, 30);
+        fifthTimerId = setInterval(rotateCat, 60);
     }
 };
 const timerID = setInterval(catWalk, 25);
 
-const ufo = document.getElementById('ufo');
-ufo.style.position = 'absolute';
-ufo.style.left = '-4500px';
-ufo.style.top = '0px';
-ufo.style.width = '800px'
+const catUFOAbduct = function () {
+    const oldBottom = parseInt(cat.style.bottom);
+    const newBottom = oldBottom + 1;
+    cat.style.bottom = newBottom + 'px';
+    if (cat.style.bottom === '560px') {
+        clearInterval(thirdTimerId);
+    } 
+};
 
-const ufoFly = function (){
+const fadeAway = function () {
+    cat.style.opacity = cat.style.opacity - 0.005;
+    if (cat.style.bottom === '560px') {
+        clearInterval(fourthTimerId);
+    } 
+};
+
+let deg = 0;
+const rotateCat = function () {
+    cat.style.transform = 'rotate(' + deg + 'deg)';
+    deg = (deg - 10) % 360;
+    if (cat.style.bottom === '560px') {
+        clearInterval(fifthTimerId);
+        sixthTimerId = setInterval(ufoFlyAway, 5);
+    } 
+}
+
+const ufoFly = function () {
     const oldLeft = parseInt(ufo.style.left);
     const newLeft = oldLeft + 1;
     ufo.style.left = newLeft + 'px';
@@ -34,33 +67,26 @@ const ufoFly = function (){
 };
 const secondTimerId = setInterval(ufoFly, 1);
 
-const ufoBeamAppear = function (){
-    document.getElementById('ufo-beam').style.visibility = 'visible';
+const ufoFlyAway = function () {
+    const oldUFOBottom = parseInt(ufo.style.bottom);
+    const newUFOBottom = oldUFOBottom + 1;
+    ufo.style.bottom = newUFOBottom + 'px';
+    if (ufo.style.bottom === '2000px') {
+        clearInterval(sixthTimerId);
+    }
 }
-setTimeout(ufoBeamAppear, 20000);
 
-const img = document.getElementsByTagName('img')[0]; 
-img.style.opacity = 1.0;
-
-
-const catUFOAbduct = function (){
-    const oldBottom = parseInt(cat.style.bottom);
-    const newBottom = oldBottom + 1;
-    cat.style.bottom = newBottom + 'px';
-    setInterval(function() {
-        img.style.opacity = img.style.opacity - 0.005;
-    }, 5000);
-    let deg = 0;
-    setInterval(function() {
-        cat.style.transform = 'rotate(' + deg + 'deg)';
-        deg -= 2;
-    }, 200); 
+const ufoBeamAppear = function () {
+    document.getElementById('ufo-beam').style.visibility = 'visible';
     if (cat.style.bottom === '560px') {
-        clearInterval(thirdTimerId);
+        document.getElementById('ufo-beam').style.visibility = 'hidden';
     } 
-};
+}
+setTimeout(ufoBeamAppear, 20050);
 
-const fadeAway = function() {
-    cat.style.opacity = cat.style.opacity - 0.005;
-};
-  
+const ufoBeamDisappear = function () {
+    document.getElementById('ufo-beam').style.visibility = 'hidden';
+}
+setTimeout(ufoBeamDisappear, 27000);
+
+
