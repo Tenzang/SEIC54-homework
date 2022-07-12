4dd4633212815bd5269168b0f70f223ed46dc419
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-07-12 23:59:10 +1000

    rip

--- yasmin-archibald/wk04/2-tues/MTA_Ruby/here_we_go_again.rb
+++ yasmin-archibald/wk04/2-tues/MTA_Ruby/here_we_go_again.rb
@@ -1,12 +1,12 @@
 require 'pry' #this is your scripts src :)
 
-lines = { "nLine" => { :stops => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],} }
+lines = { "nLine" => { :stops => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],}, "lLine" => {:stops => ["8th", "6th", "Union Square", "3rd", "1st"]}, }
 
 def my_trip
     puts "Welcome to My Trip"
     puts "Please select from the following menu"
-    puts "[2] - Line Trip"
     puts "[1] - 1 Line Trip"
+    puts "[2] - Line Trip"
     puts "[Q] - Quit"
 end
 my_trip
@@ -15,76 +15,75 @@ until trip_choice == 'q'
     case trip_choice
 ##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//
     when "1"
+            puts "Enter your line"
+            line = gets.chomp ## worry about capitalisation etc later..
             puts "Enter your starting stop. : "
             starting = gets.split.map(&:capitalize).join(" ")
         
             puts "Enter your final stop. More lines coming soon: "
             final = gets.split.map(&:capitalize).join(" ")
-            
-            starting_index = lines["nLine"][:stops].index(starting)
-         
-            final_index = lines["nLine"][:stops].index(final)
-         
+            ##index
+            starting_index = lines[line][:stops].index(starting)
+            final_index = lines[line][:stops].index(final)
+            ##backwards trip
             if final_index < starting_index
-            reverse_line_stops = lines["nLine"][:stops].reverse
+            reverse_line_stops = lines[line][:stops].reverse
             reverse_starting_index = reverse_line_stops.index(starting)
             reverse_final_index = reverse_line_stops.index(final)
             puts "Get on #{starting} traveling through #{reverse_line_stops[reverse_starting_index...reverse_final_index]} depart at #{final}"
-            else 
-            puts "Get on #{starting} traveling through #{lines["nLine"][:stops][starting_index...final_index]} depart at #{final}"
+            else ##forwards trip
+            puts "Get on #{starting} traveling through #{lines[line][:stops][starting_index...final_index]} depart at #{final}"
             end
+            when "2"
+                puts "Enter first line: "
+                first_line = gets.chomp ## worry about capitalisation etc later
+                puts "Enter first station: "
+                starting = gets.split.map(&:capitalize).join(" ")
+                puts "Enter second line: "
+                second_line = gets.chomp ## worry about capitalisation etc later
+                puts "Enter last station: "
+                final = gets.split.map(&:capitalize).join(" ")
+                ##index
+                starting_index = lines[first_line][:stops].index(starting)
+                final_index = lines[second_line][:stops].index(final)
+                ## Union Sq index's
+                first_USR_index = lines[first_line][:stops].index("Union Square")
+                second_USR_index = lines[second_line][:stops].index("Union Square")
+                ## First Half Second Half Trip Variables
+                first_half = ""
+                second_half = ""
+                if first_USR_index < starting_index ##backward trip 1st half
+                    reverse_line_stops = lines[first_line][:stops].reverse
+                    reverse_starting_index = reverse_line_stops.index(starting)
+                    reverse_USR_index = reverse_line_stops.index("Union Square")
+                    first_half = "Get on #{starting} traveling through #{reverse_line_stops[reverse_starting_index...reverse_USR_index]} change at Union Square"
+                    return first_half
+                    # puts "Your trip on #{first_line} #{first_half}."
+                else ##forwards trip first half
+                     first_half = "Get on #{starting} traveling through #{lines[first_line][:stops][starting_index...first_USR_index]} change at Union Square"
+                     return first_half
+                    #  puts "Your trip on #{first_line} #{first_half}."
+                if second_USR_index < final_index ##backwards trip 2nd half   
+                    reverse_line_stops = lines[second_line][:stops].reverse
+                    reverse_final_index = reverse_line_stops.index(final)
+                    reverse_USR_index = reverse_line_stops.index("Union Square")
+                    second_half = "Get on at Union Square travele through #{reverse_line_stops[reverse_USR_index...reverse_final_index]} change at Union Square"
+                    return second_half
+                    # "Your trip concludes on #{second_line} #{second_half}"
+                else ##forwards trip second half
+                    second_half = "Get on at Union Square traveling through #{lines[second_line][:stops][second_USR_index...final_index]} change at Union Square"
+                    return second_half
+                    # "Your trip concludes on #{second_line} #{second_half}"
+                # puts "Your trip on #{first_line} #{first_half}. Your trip concludes on #{second_line} #{second_half}"
+        
+                # lines["nLine"][:stops]
+            end
+        end
+        
     else puts "Invalid choice, invalid lifeform"
     end
 my_trip
 trip_choice = gets.chomp.downcase
 end
-
-
-
-# else 
-#     if starting_index = (final_index+1)
-#         puts "Get on #{starting}, depart at your next stop #{final}"
-#     else
-#     puts "Get on #{starting} traveling through #{lines["nLine"][:stops][starting_index+1..final_index-1]} depart at #{final}"
-#     end
-# end
-# trip_choice
-# trip_choice = gets.chomp.downcase
-# end
-
-# def two_Line_Trip 
-#     puts "What is the train line you begin on: "
-#     puts "[N] - Nline"
-#     puts "[L] - Lline"
-#     puts "[6] - 6line"
-#     starting_line = gets.chomp.downcase
-#     puts "What is the train line you end on: "
-#     final_line = gets.chomp.downcase
-# end
-
-# def my_trip
-#     puts "Welcome to My Trip"
-#     puts "Please select from the following menu"
-#     puts "[2] - Line Trip"
-#     puts "[1] - 1 Line Trip"
-#     puts "[Q] - Quit"
-# end
-
-
-# my_trip
-# trip_choice = gets.chomp.downcase
-
-# until trip_choice == 'q'
-#     case trip_choice
-#     when '1'
-#         one_line_trip
-#     when '2'
-#         two_Line_Trip
-# else
-#     puts "Invalid Selection"
-# end
-# trip_choice
-# trip_choice = gets.chomp.downcase
-# end 
 
  binding.pry  
