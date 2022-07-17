78082d36db99e4006244b91da77b4601f86c404f
Christopher Holt <holt.christopher1@gmail.com>
2022-07-17 16:49:47 +1000

    fixing up the database for active record, adding the pokemon moves, making it look prettier

---
+++ christopher-holt/wk04/5-fri/moves/create_pokemons.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pokemons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    types TEXT,
+    image TEXT,
+    level TEXT,
+    HP TEXT,
+    Attack TEXT,
+    Defense TEXT,
+    Special TEXT,
+    Speed TEXT
+);
