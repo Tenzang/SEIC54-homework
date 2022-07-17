ea9c15ded7c01ce2204ad453005277cbd4c443dd
Edward Do <edwardtdo@gmail.com>
2022-07-17 21:46:41 +1000

    sql homework

--- edward-do/wk04/5-fri/create_plants.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE plants (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    image TEXT
-);
-
--- seed data
-INSERT INTO plants (name) VALUES ('Milkweed');
-INSERT INTO plants (name) VALUES ('Lantana');
