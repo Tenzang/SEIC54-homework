087a2e7952093d173f91de364a8ca72fd8cf17e4
Elias Khoury <eliaskhouryy@hotmail.com>
2022-07-19 21:06:19 +1000

    hw

---
+++ elias-khoury/wk05/2-tues/mountains/app/controllers/mountains_controller.rb
@@ -0,0 +1,38 @@
+class MountainsController < ApplicationController
+    def index
+        @mountains = Mountain.all
+    end
+
+    def show
+        @mountain = Mountain.find params[:id]
+    end
+
+    def new
+    end
+
+    def create
+        mountain = Mountain.create :name => params[:name], :image => params[:image], :country => params[:country], :height => [:height] 
+        redirect_to mountain_path(mountain.id)
+    end
+
+    def edit
+        @mountain = Mountain.find params[:id]
+    end
+
+    def update
+        mountain = Mountain.find params[:id]
+        mountain.name = params[:name]
+        mountain.image = params[:image]
+        mountain.country = params[:country]
+        mountain.height = params[:height]
+        mountain.save
+        redirect_to mountain_path(mountain.id)
+    end
+
+    def destroy
+        mountain = Mountain.find params[:id]
+        mountain.destroy
+        redirect_to mountains_path
+    end
+
+end
