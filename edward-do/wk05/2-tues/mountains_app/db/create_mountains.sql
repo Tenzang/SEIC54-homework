b1f51683ef50e05a5ea699bdd2e12f7ddb85adec
edward <edwardtdo@gmail.com>
2022-07-19 22:30:24 +1000

    unfinished week 5 tuesday homework

---
+++ edward-do/wk05/2-tues/mountains_app/db/create_mountains.sql
@@ -0,0 +1,9 @@
+CREATE TABLE mountains (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    native_name TEXT,
+    country TEXT,
+    image TEXT,
+    elevation INTEGER,
+    parent_range TEXT
+);
