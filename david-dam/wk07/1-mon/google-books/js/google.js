8f363ccbe1b6110283a98374488ea6cb919d1d0d
David Dam <dkdam1302@gmail.com>
2022-08-04 23:51:13 +1000

    Sorting out my recursions

--- david-dam/wk07/1-mon/google-books/js/google.js
+++ david-dam/wk07/1-mon/google-books/js/google.js
@@ -7,20 +7,23 @@ const search = function () {
     xhr.send();
     xhr.onreadystatechange = function () {
 
+        //You need this for 
         if (xhr.readyState != 4) {
             return;
         }
     
-
+        //Grabs the Image
         const imageElement = document.getElementById('cover');
         const data = JSON.parse(xhr.responseText);
         const thumbnail = data.items[1].volumeInfo.imageLinks.thumbnail;
         imageElement.innerHTML = `<img src="${ thumbnail }" alt="searched novels cover">`;
 
+        //Grabs the title
         const titleElement = document.getElementById('title');
         const title = data.items[0].volumeInfo.title 
         titleElement.innerHTML = `<h1>${ title }</h1>`
 
+        //Grabs the Description
         const descriptionElement = document.getElementById('description')
         const description = data.items[1].volumeInfo.description 
         descriptionElement.innerHTML = `<p>${ description }</p>`
---
+++ david-dam/wk07/4-thurs/basic-sorts/BubbleSort.js
@@ -0,0 +1,31 @@
+function bubbleSort(array) {
+
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < (array.length - i -1); j++)
+        if(array[j] > array[j+1]){
+            let temp = array[j]
+            array[j] = array[j + 1]
+            array[j + 1] = temp
+        }
+    }
+    return array;
+}
+
+module.exports = bubbleSort;
+
+    // Make some sort of loop that keeps happening if there were any swaps
+    // that happened this turn
+
+    // reset swapHappened to false each time so we can detect if a swap
+    // happened in this specific iteration.
+
+    // Make another loop (inside the first one) to go through one 
+    // round of swapping from the start of the list to the end
+    
+    // Inside inner loop:
+    // compare each pair of elements near each other
+    // swap them if the bigger one was at a lower index.
+
+    // Make sure to keep track of whether a swap happened!
+
+    // After both loops have exited, remember to return the array
