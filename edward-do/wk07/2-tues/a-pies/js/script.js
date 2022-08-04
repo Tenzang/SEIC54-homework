4dce3570e4a5aebeec4e05b8f304a886bce8b504
Edward Do <edwardtdo@gmail.com>
2022-08-04 16:55:14 +1000

    hoe work

---
+++ edward-do/wk07/2-tues/a-pies/js/script.js
@@ -0,0 +1,43 @@
+console.log('is this thing on?')
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://lingua-robot.p.rapidapi.com/language/v1/entries/en/",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "5a30312a0cmshd06400ccc8f6806p163943jsnd3b498b8bed2",
+		"X-RapidAPI-Host": "lingua-robot.p.rapidapi.com"
+	}
+};
+
+json = {};
+
+const fetchWord = function (word) {
+    $.ajax(settings.url + word, settings).done(function (data) {
+        json = data;
+        const lemma = data.entries[0].lexemes[0].lemma
+        const partOfSpeech = data.entries[0].lexemes[0].partOfSpeech
+        const definition = data.entries[0].lexemes[0].senses[0].definition
+
+        $('#lemma').text(lemma)
+        $('#part-of-speech').text(partOfSpeech)
+        $('#definition').text(definition)
+    });
+};
+
+$('#search').on('submit', function (event) {
+    event.preventDefault();
+
+    const word = $('#word').val();
+    fetchWord(word);
+})
+
+$.ajax('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud.json').done(function (data) {
+    $('#usd').text(`${data.aud.usd} USD`)
+    $('#eur').text(`${data.aud.eur} euros`);
+});
+
+// json.entries[0].lexemes[0].lemma
+// json.entries[0].lexemes[0].partOfSpeech
+// json.entries[0].lexemes[0].senses
---
+++ edward-do/wk07/3-wed/js/search.js
@@ -0,0 +1,85 @@
+// move global variables into their namespace
+const state = {
+    currentPage: 1,
+    lastPageReached: false,
+    requestInProgress: false
+};
+
+const searchFlickr = function (keywords) {
+
+    if (state.lastPageReached || state.requestInProgress) return;
+
+    console.log('Searching for', keywords);
+
+    // get the data 
+    const flickrURL = 'https://api.flickr.com/services/rest';
+
+    state.requestInProgress = true;
+    $.getJSON(flickrURL, {
+        method: 'flickr.photos.search',
+        api_key: '2f5ac274ecfac5a455f38745704ad084',
+        text: keywords,
+        format: 'json',
+        page: state.currentPage++,
+        nojsoncallback: 1 // please don't be bored enough to read up this
+    }).done(showImages).done(function (response) {
+        console.log(response);
+        if (response.photos.page >= response.photos.pages) {
+            state.lastPageReached = true;
+        }
+        state.requestInProgress = false;
+    });
+    // show the data
+};
+
+const showImages = function (results) {
+    _(results.photos.photo).each(function (photo) {
+        // generate a thumbnail
+        const thumbnail = generateURL(photo);
+        const $img = $('<img>', {src: thumbnail});
+        $('#images').append($img);
+        // display the thumbnail
+    });
+};
+
+const generateURL = function (p) {
+    return [
+        'http://farm',
+        p.farm,
+        '.static.flickr.com/',
+        p.server,
+        '/',
+        p.id,
+        '_',
+        p.secret,
+        '_q.jpg' // change 'q' to something else for different sizes (see documentation)
+    ].join('');
+};
+
+$(document).ready(function () {
+    $('#search').on('submit', function (event) {
+        event.preventDefault(); // disable the form from being submitted to a server.
+
+        $('#images').empty();
+        state.currentPage = 1;
+        state.lastPageReached = false;
+
+        const searchTerms = $('#query').val();
+        searchFlickr(searchTerms);
+    });
+
+    // Higher Order Function
+    const relaxedSearchFlickr = _.debounce(searchFlickr, 2000, true); // true: leading edge (don't wait to run)
+
+    $(window).on('scroll', function () {
+        // calculate the scrollBottom (how close we are to the end of the document)
+        const scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+        // console.log(scrollBottom);
+        // if (scrollBottom < 600)
+            // searchFlickr again for more photos
+        if (scrollBottom < 600) {
+            const searchTerms = $('#query').val();
+            relaxedSearchFlickr(searchTerms);
+        };
+    });
+});
