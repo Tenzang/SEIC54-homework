bce3c233af24c35db87fbf3f4bd2fd4c9f67cb8e
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-08-06 21:16:59 +1000

    progress on timetravel -  game class update

--- yasmin-archibald/wk07/5-fri/naughts-and-crosses/src/index.js
+++ yasmin-archibald/wk07/5-fri/naughts-and-crosses/src/index.js
@@ -61,35 +61,52 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-    handleClick(i) {
-        const squares = this.state.squares.slice()
-        if (calculateWinner(squares) || squares[i]) {
-            return
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      xIsNext: true,
-    };
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
-  render() {
+
+  renderSquare(i) {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    return (
+      <div>
+        <div className="status"> {status} </div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/*TODO*/}</ol>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
