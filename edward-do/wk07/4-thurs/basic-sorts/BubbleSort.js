3ff06ec00fec7333cad6d0943d6c1075c6ee9ee1
Edward Do <edwardtdo@gmail.com>
2022-08-06 17:52:04 +1000

    week7fri

--- edward-do/wk07/4-thurs/basic-sorts/BubbleSort.js
+++ edward-do/wk07/4-thurs/basic-sorts/BubbleSort.js
@@ -1,38 +1,36 @@
 function bubbleSort(array) {
-    // Make some sort of loop that keeps happening if there were any swaps
-    // that happened this turn
+    // for (let i = 0; i < array.length; i++) {
+    //     // Last i elements are already in place 
+    //     for (let j = 0; j < ( array.length - i - 1 ); j++) {  
+    //         // Checking if the item at present iteration
+    //         // is greater than the next iteration
+    //         if (array[j] > array[j + 1]) {
+    //             // If the condition is true then swap them
+    //             let temp = array[j]
+    //             array[j] = array[j + 1]
+    //             array[j + 1] = temp
+    //         }
+    //     }
+    // }
+    // return array;
 
-    // reset swapHappened to false each time so we can detect if a swap
-    // happened in this specific iteration.
+    let needToIterate = true;
 
-    // Make another loop (inside the first one) to go through one 
-    // round of swapping from the start of the list to the end
-    
-    // Inside inner loop:
-    // compare each pair of elements near each other
-    // swap them if the bigger one was at a lower index.
+    let end = array.length - 1;
 
-    // Make sure to keep track of whether a swap happened!
-
-    // After both loops have exited, remember to return the array
-
-    for (let i = 0; i < array.length; i++) {
-        // Last i elements are already in place 
-        for (let j = 0; j < ( array.length - i - 1 ); j++) {  
-            // Checking if the item at present iteration
-            // is greater than the next iteration
-            if (array[j] > array[j + 1]) {
-                // If the condition is true then swap them
-                let temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
+    while (needToIterate) {
+        needToIterate = false; // assume we are done
+        for (let i = 0; i < end; i++) {
+            if (array[i] > array[i+1]) {
+                // destructuring parallel assignment: [a, b] = [b, a];
+                [array[i], array[i+1]] = [array[i+1], array[i]];
+                needToIterate = true;
             }
         }
+        end--;
     }
+
     return array;
 }
 
 module.exports = bubbleSort;
-
-// [4, 2, 3, 1, 5]
-//  i i+1
