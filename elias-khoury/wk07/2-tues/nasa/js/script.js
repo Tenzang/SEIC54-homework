fbd8209cd00d04616a9eb0a69407b75e54c35041
Elias Khoury <eliaskhouryy@hotmail.com>
2022-08-08 17:47:05 +1000

    big update

--- elias-khoury/wk07/2-tues/nasa/js/script.js
+++ elias-khoury/wk07/2-tues/nasa/js/script.js
@@ -1,34 +1,27 @@
-const beautifulImage = function() {
-    $.ajax('https://api.nasa.gov/planetary/apod?api_key=ZCJEhiXYm9T69pxcLZJxOvwhNVA7diTZhJbuGjKd').done(function(data){
-        const title = data.title
-        const picture = data.url
-        const info = data.explanation
-        $('#title').html(title)
-        $('#image').attr('src', picture)
-        $('#description').html(info)
-    })
-}
+const beautifulImage = function () {
+    $.ajax("https://api.nasa.gov/planetary/apod?api_key=ZCJEhiXYm9T69pxcLZJxOvwhNVA7diTZhJbuGjKd").done(function (data) {
+        const title = data.title;
+        const picture = data.url;
+        const info = data.explanation;
+        $("#title").html(title);
+        $("#image").attr("src", picture);
+        $("#description").html(info);
+    });
+};
 
-$('#button').on('click', beautifulImage)
+$("#button").on("click", beautifulImage);
 
-
-
-
-
-
-const randomFacts = function() {
+const randomFacts = function () {
     const limit = $(`#factsAmount`).val();
     $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/facts?limit=' + limit,
-        headers: { 'X-Api-Key': 'P6HbU6FOC20yEWLsWX10iw==w8M7VCnzetOzbMzm'},
-        contentType: 'application/json',
-        success: function(result) {
-            
+        method: "GET",
+        url: "https://api.api-ninjas.com/v1/facts?limit=" + limit,
+        headers: { "X-Api-Key": "P6HbU6FOC20yEWLsWX10iw==w8M7VCnzetOzbMzm" },
+        contentType: "application/json",
+        success: function (result) {
             if (limit < 2) {
                 $(`#random1`).text(result[0].fact);
                 console.log(result[0].fact);
-
             } else if (limit === 2) {
                 $(`#random1`).text(result[0].fact);
                 $(`#random2`).text(result[1].fact);
@@ -44,15 +37,14 @@ const randomFacts = function() {
             }
         },
         error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
+            console.error("Error: ", jqXHR.responseText);
+        },
     });
-}
+};
 
-$('#factsForm').submit(function(event){
+$("#factsForm").submit(function (event) {
     randomFacts();
     event.preventDefault();
 });
-
 
 // $('#button2').on('click', randomFacts)
