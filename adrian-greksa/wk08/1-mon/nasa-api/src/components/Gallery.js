59cbbdaefd8623bd887c28057987e3774e5e0dc4
Adrian Greksa <adrian.greksa@gmail.com>
2022-08-09 22:33:08 +1000

    nasa-api REACT

---
+++ adrian-greksa/wk08/1-mon/nasa-api/src/components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>Images from nasa api go here</h1>
+        </div>
+    );
+};
+
+export default Gallery;
---
+++ adrian-greksa/wk08/1-mon/nasa-api/src/components/SearchForm.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const SearchForm = (props) => {
+    const [query, setQuery] = useState('');
+
+    const _handleSubmit = (e) => {
+        e.preventDefault();
+        props.onSubmit(query);
+    };
+
+    const _handleInput = (e) => {
+        setQuery(e.target.value);
+    };
+
+    return (
+        <form onSubmit={ _handleSubmit } >
+            <input type="search" required placeholder="orion" onInput={ _handleInput } />
+            <input type="submit" value={ `Search for ${ query }` } />
+        </form>
+    );
+};
+
+export default SearchForm;
