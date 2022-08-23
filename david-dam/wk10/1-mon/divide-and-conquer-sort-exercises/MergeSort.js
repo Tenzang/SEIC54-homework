ccf1c52cd9bafe8c14d6f2420fb8ce3ace4a3a72
dkdam <dkdam@live.com>
2022-08-24 09:38:40 +1000

    submitting MY hw

--- david-dam/wk10/1-mon/divide-and-conquer-sort-exercises/MergeSort.js
+++ david-dam/wk10/1-mon/divide-and-conquer-sort-exercises/MergeSort.js
@@ -1,4 +1,5 @@
 function mergeSort(arr) {
+
   if (arr.length <= 1) {
 		return arr;
 	}
@@ -8,6 +9,20 @@ function mergeSort(arr) {
 	const rightArr = arr.slice(midPoint);
 
 	return merge(mergeSort(leftArr), mergeSort(rightArr));
+
+  // ----------------------------------------------------------------------
+
+  // if (arr.length <= 1) {
+	// 	return arr;
+	// } else {
+  //   const middleIndex = Math.floor(arr.length / 2);
+  //   const left = arr.slice(0, middleIndex);
+  //   const right = arr.slice(middleIndex);
+
+  //   const sortedLeft = mergeSort(left);
+  //   const sortedRight = mergeSort(right)
+  // }
+
 }
 
 
